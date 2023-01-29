bca4abc96652702f25f985da2acc37b46b2cd172
DeX-Coder <dehimangshu03@gmail.com>
2023-01-29 10:14:34 +0530

    Completed Dummy Data Driven Blog Site
    Updated business logic in views.
    
    Updated routes.
    
    Modified all-post related and starting page html files.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import starting_page, posts, post_detail
 
 urlpatterns = [
-    path("", starting_page, name='starting-page'),
-    path("posts", posts, name='posts'),
-    path("posts/<slug:slug>",post_detail, name='post-detail') # /post/my-first-post
+    path("", starting_page, name="starting-page"),
+    path("posts", posts, name="posts"),
+    path("posts/<slug:slug>", post_detail, name="post-detail"),  # /post/my-first-post
 ]

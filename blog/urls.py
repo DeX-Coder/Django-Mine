5877613ff14bbee83af8fa079d3a3d39dd8519da
DeX-Coder <dehimangshu03@gmail.com>
2023-02-16 21:03:57 +0530

    Initial Commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import starting_page, posts, post_detail
+
+urlpatterns = [
+    path("", starting_page, name="starting-page"),
+    path("posts", posts, name="posts"),
+    path("posts/<slug:slug>", post_detail, name="post-detail"),  # /post/my-first-post
+]

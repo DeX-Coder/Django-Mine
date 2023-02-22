6d0f936d91afd5f4048592897ce1119105bb0a2f
DeX-Coder <dehimangshu03@gmail.com>
2023-02-22 19:49:30 +0530

    Modifications Made!
    ⋄Whats New
    ⇨ All the data will be fetched from models.
    
    ⇨ Data creation is possible via admin interface or interactive console.
    
    ⌘ Dev Zone:
    Migration Files are added. Migrate before firing the project!

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Tags(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        verbose_name_plural = "Tags"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(null=False)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts", null=True
+    )
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tag = models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.title
